2357e21299c012c27849b00166bb83c84916fe59
teach-beast <tech.beast130@gmail.com>
2024-02-21 00:15:14 -0600

    add create post component

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,12 @@
+import CreatePostInput from "@/components/CreatePostInput";
+import ReplyInput from "@/components/ReplyInput";
 import React from "react";
 
 export default function Test() {
   return (
     <div>
-      <Typography>Hello world</Typography>
+      <ReplyInput />
+      <CreatePostInput />
     </div>
   );
 }
--- src/components/CreatePostInput.tsx
+++ src/components/CreatePostInput.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
-const ReplyInput = () => {
+const CreatePostInput = () => {
   return (
     <div className="my-4 flex w-full rounded-xl border border-[#E5E7EB] p-4 pb-3 shadow-md shadow-gray-100">
       <div className="mr-4 flex items-start justify-start">
@@ -19,13 +19,14 @@ const ReplyInput = () => {
       </div>
       <div className="flex flex-grow flex-col">
         <Textarea
-          placeholder="Comment your thoughts"
-          id="user-post-commit"
+          placeholder={
+            "Title of your post\nShare your thoughts with the world!"
+          }
           className="resize-none rounded-none border-0 border-b border-gray-300 p-0 text-base focus-visible:border-gray-400 focus-visible:ring-0 focus-visible:ring-offset-0"
         />
         <div className="flex w-full justify-end ">
           <Button className="mt-2 rounded-lg px-4 py-2 text-sm text-white">
-            Comment
+            Post
           </Button>
         </div>
       </div>
@@ -33,4 +34,4 @@ const ReplyInput = () => {
   );
 };
 
-export default ReplyInput;
+export default CreatePostInput;
