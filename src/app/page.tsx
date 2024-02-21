5e70a116d6551a5ddee8caf5179f36e1d1737c0a
teach-beast <tech.beast130@gmail.com>
2024-02-21 14:12:04 -0500

    auth api changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,20 @@ import { Separator } from "@/components/ui/separator";
 import Sidebar from "@/components/Sidebar";
 import PostList from "@/components/PostList";
 
-// import { api } from "@/trpc/server";
+import { api } from "@/trpc/server";
 
 export default async function Home() {
   noStore();
+
+  // const user = api.user.auth.mutate({
+  //   email: 'anthony.bartolotte.1010@gmail.com',
+  //   name: 'Anthony Bartolotte',
+  //   avatar: 'https://www.no.avatar.com/m.png'
+  // });
+
+  // const user = api.user.get.query({ id: 1 });
+
+  // console.log(user);
 
   return (
     <main className="flex w-full min-h-screen">
