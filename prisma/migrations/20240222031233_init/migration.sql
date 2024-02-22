aa1f3a8d5c9aecd74593ed3c3b9bdc83b7871615
teach-beast <tech.beast130@gmail.com>
2024-02-21 22:36:09 -0500

    api updated for vote users

---
+++ prisma/migrations/20240222031233_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_id_key`(`id`),
+    UNIQUE INDEX `User_email_key`(`email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `votes` INTEGER NOT NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+    `parentId` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Vote` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(191) NOT NULL,
+    `postId` INTEGER NOT NULL,
+    `score` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 // Recursive function to fetch nested children posts
-async function fetchNestedChildren(db: any, postId: number) {
+const fetchNestedChildren = async (db: any, postId: number) => {
   const children = await db.post.findMany({
     where: {
       parentId: postId,
@@ -17,14 +17,21 @@ async function fetchNestedChildren(db: any, postId: number) {
   const nestedChildren = await Promise.all(
     children.map(async (child: any) => {
       const grandchildren = await fetchNestedChildren(db, child.id);
+      const voteUsers = await fetchVoteUsers(db, postId);
       return {
         ...child,
         children: grandchildren,
+        voteUsers
       };
     }),
   );
 
   return nestedChildren;
+}
+
+const fetchVoteUsers = async (db: any, postId: number) => {
+  const voteUsers = await db.vote.findMany({ where: { postId } });
+  return voteUsers;
 }
 
 export const postRouter = createTRPCRouter({
@@ -60,7 +67,11 @@ export const postRouter = createTRPCRouter({
             parentId: null // Fetch only top-level posts
           }
         });
-        return posts.reverse();
+        const postsWithVoteUsers = posts.map(async (post) => ({
+          ...post,
+          voteUsers: await fetchVoteUsers(ctx.db, post.id)
+        }));
+        return postsWithVoteUsers.reverse();
       } catch (error) {
         console.error("Error fetching posts:", error);
         return { error: "Internal server error" };
@@ -76,9 +87,11 @@ export const postRouter = createTRPCRouter({
         const post = await ctx.db.post.findUnique({ where: { id } });
         if (post) {
           const nestedChildren = await fetchNestedChildren(ctx.db, post.id);
+          const voteUsers = await fetchVoteUsers(ctx.db, post.id);
           return {
             ...post,
-            children: nestedChildren
+            children: nestedChildren,
+            voteUsers
           };
         }
         return { error: "Post not found" };
@@ -117,5 +130,46 @@ export const postRouter = createTRPCRouter({
         where: { id: input.id },
       });
       return { message: `Post with ID ${input.id} deleted successfully.` };
+    }),
+
+  votePost: publicProcedure
+    .input(z.object({
+      id: z.number(),
+      score: z.number(),
+      userId: z.string().min(1)
+    }))
+    .mutation(async ({ ctx, input }) => {
+      const { id, score, userId } = input;
+      try {
+        const [post, user, vote] = await Promise.all([
+          ctx.db.post.findUnique({ where: { id } }),
+          ctx.db.user.findUnique({ where: { id: userId } }),
+          ctx.db.vote.findUnique({ where: { voteId: { postId: id, userId } } })
+        ]);
+
+        if (!post || !user) {
+          throw new Error('Post or user not found');
+        }
+
+        await ctx.db.$transaction(async (tx) => {
+          if (vote) {
+            const updatedPostVotes = post.votes - vote.score + score;
+            await Promise.all([
+              tx.post.update({ where: { id }, data: { votes: updatedPostVotes } }),
+              tx.vote.update({ where: { id: vote.id }, data: { score } })
+            ]);
+            return { message: 'Vetoed successfully.' };
+          } else {
+            await Promise.all([
+              tx.post.update({ where: { id }, data: { votes: { increment: score } } }),
+              await tx.vote.create({ data: { userId: user.id, postId: post.id, score } })
+            ]);
+            return { message: 'Voted successfully.' };
+          }
+        });
+      } catch (error) {
+        console.error('Error in votePost:', error);
+        throw new Error('Failed to vote or veto');
+      }
     }),
 });
